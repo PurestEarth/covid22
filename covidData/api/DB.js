6ca693c797c6851463c90f41ec30d797f6880119
PurestEarth <jaremaradom@gmail.com>
2020-04-25 16:47:49 +0200

    setup

---
+++ covidData/api/DB.js
@@ -0,0 +1,5 @@
+var host=process.env.DB_HOST||'localhost'
+module.exports = {
+    //  DB: 'mongo:21131/showcase'
+    DB: 'mongodb://localhost:27017/covid'
+};
---
+++ covidData/api/server.js
@@ -0,0 +1,28 @@
+
+const express = require('express'),
+cors = require('cors'),
+bodyParser = require('body-parser'),
+mongoose = require('mongoose'),
+bearerToken = require('express-bearer-token'),
+config = require('./DB');
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+() => {console.log('Database is connected') },
+err => { console.log('Can not connect to the database'+ err)}
+);
+const app = express();
+app.use(cors());
+
+app.use(bearerToken());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: false
+}));
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, function(){
+console.log('Listening on port ' + port);
+});
+
+
